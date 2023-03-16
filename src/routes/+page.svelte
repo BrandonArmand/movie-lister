968f20fbc683b48bf4165539e52c1795080963f2
brandon welsh <contact@brandonarmand.com>
2023-03-16 08:14:48 +0000

    init setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let data;
+    import MovieTile  from './MovieTile.svelte';
+</script>
+
+<div class="container">
+{#each data.movies as movie}
+    <MovieTile img={movie.poster_path} title={movie.title} date={movie.release_date}/>
+{/each}
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+</style>
---
+++ src/routes/MovieTile.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    export let img: string;
+    export let title: string;
+    export let date: string;
+</script>
+
+<div class="tile">
+    <img src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${img}`} alt={title}/>
+    <div class="details">
+        <h4>{title}</h4>
+        <p>{date}</p>
+    </div>
+</div>
+
+<style>
+    .tile {
+        width: 10rem;
+        height: auto;
+        margin: 5px;
+        padding: 5px;
+        background-color: #fff;
+        border-radius: 5px;
+        box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+    }
+
+    .tile img {
+        width: 100%;
+        height: auto;
+    }
+
+    .details {
+        height: 5rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .details h4, p {
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+</style>
